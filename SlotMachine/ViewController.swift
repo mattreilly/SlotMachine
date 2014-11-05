2f27245109b8daeb26a010c85c334d322e506298
Matt Reilly <matt@matteroo.com>
2014-11-05 14:19:11 -0500

    initial project setup, created container variables

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var firstContainer: UIView!
+    var secondContainer: UIView!
+    var thirdContainer: UIView!
+    var fouthContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
