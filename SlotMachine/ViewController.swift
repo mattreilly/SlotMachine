9d775a71e533138d4911446e2ce4f954bfd175bc
Matt Reilly <matt@matteroo.com>
2014-11-05 21:07:40 -0500

    finished setup of second container

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -18,14 +18,21 @@ class ViewController: UIViewController {
     var titleLabel: UILabel!
     
     let kMarginForView:CGFloat = 10.0
+    let kMarginForSlot:CGFloat = 2.0
+
     let kSixth:CGFloat = 1.0/6.0
+    let kThird:CGFloat = 1.0/3.0
     
+    let kNumberOfContainers = 3
+    let kNumberOfSlots = 3
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
         setupContainerViews()
         setupFirstContainer(self.firstContainer)
+        setupSecondContainer(self.secondContainer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -87,6 +94,34 @@ class ViewController: UIViewController {
         self.titleLabel.center = containerView.center
 
         containerView.addSubview(self.titleLabel)
+    }
+
+    func setupSecondContainer(containerView: UIView) {
+         
+        for var containerNumber = 0; containerNumber < kNumberOfContainers;
+        ++containerNumber {
+            
+            for var slotNumber = 0; slotNumber < kNumberOfSlots; ++slotNumber {
+                
+                var slotImageView = UIImageView()
+                slotImageView.backgroundColor = UIColor.yellowColor()
+
+                slotImageView.frame = CGRect(
+                    x: containerView.bounds.origin.x +
+                      (containerView.bounds.size.width *
+                       CGFloat(containerNumber) * kThird),
+                    y: containerView.bounds.origin.y +
+                      (containerView.bounds.size.height *
+                       CGFloat(slotNumber) * kThird),
+                    width: (containerView.bounds.width * kThird) -
+                            kMarginForSlot,
+                    height: (containerView.bounds.height * kThird) -
+                             kMarginForSlot
+                )
+
+                containerView.addSubview(slotImageView)
+            }
+        }
     }
 
 }
